6a000c3be9fb86279816dd8da12c522b02a816b7
Mahmoud Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2021-01-28 04:34:01 +0200

    Add files via upload

---
+++ Python/Python_Project_ATM/ATM.py
@@ -0,0 +1,12 @@
+#################################
+#              ATM              #
+# Author: Mahmoud Mohamed Kamal	#
+# Date  : 23 JAN 2021		    #
+# File  : GUI_Module.pu         #
+#################################
+
+import GUI_Modules as GUI
+
+GUI.Create_MainWindow()
+
+GUI.mainloop()
